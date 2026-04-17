20d2de078a4d36f5b075daeb0501d09e2a1b364c
Jacob Foote <jcfoote1118@gmail.com>
2026-04-17 12:22:07 -0400

    item_pickup texture updates in editor

--- Items/item_pickup/item_pickup.gd
+++ Items/item_pickup/item_pickup.gd
@@ -1,13 +1,24 @@
-extends Area2D
+@tool
+class_name ItemPickup extends Area2D
 
-@export var slot_data : SlotData
+#@export var slot_data : SlotData :
+	#set(_value):
+		#slot_data = _value
+		#update_visuals()
+
+@export var item_data : ItemData :
+	set(_value):
+		item_data = _value
+		update_visuals()
 
 var entered : bool = false
 var player : Player
 
 func _ready() -> void:
-	%Sprite2D.texture = slot_data.item_data.texture
-	self.position = grid_snap(%Sprite2D.texture.get_height())
+	update_visuals()
+	if Engine.is_editor_hint():
+		return
+	self.position = grid_snap(16)
 
 func grid_snap(size : int) -> Vector2:
 	var new_x = round(position.x / size) * size
@@ -15,7 +26,12 @@ func grid_snap(size : int) -> Vector2:
 	return Vector2(new_x,new_y)
 
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	if Input.is_action_just_pressed("interact") and entered:
+		var slot_data : SlotData = SlotData.new()
+		slot_data.item_data = item_data
 		if player.inventory_data.pick_up_slot_data(slot_data):
 			queue_free()
 
@@ -28,3 +44,8 @@ func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
 		entered = false
 		player = null
+
+func update_visuals():
+	if not item_data or not item_data.texture:
+		return
+	%Sprite2D.texture = item_data.texture
