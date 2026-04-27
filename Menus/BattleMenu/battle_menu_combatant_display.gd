81e8b3c5d79992dcd7b38855f043e9c8be886a2b
Jacob Foote <jcfoote1118@gmail.com>
2026-04-27 16:01:02 -0400

    updated battle menu and other things

--- Menus/BattleMenu/battle_menu_combatant_display.gd
+++ Menus/BattleMenu/battle_menu_combatant_display.gd
@@ -1,13 +1,21 @@
 @tool
 class_name BattleMenuCombatantDisplay extends Control
 
-@export var height : int = 32
-@export var width : int = 16
+@export var x : int = 16 :
+	set(_value):
+		x = _value
+		update_initial_visuals()
 
-var combatant : Combatant
+@export var y : int = 32 :
+	set(_value):
+		y = _value
+		update_initial_visuals()
+
+@export var combatant : Combatant :
+	set(_value):
+		combatant = _value
+		update_initial_visuals()
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
-@onready var selection_arrow: Sprite2D = %SelectionArrow
-@onready var sprite_display: Sprite2D = %SpriteDisplay
 
 
 func _ready() -> void:
@@ -17,20 +25,25 @@ func _ready() -> void:
 	#code
 
 func update_initial_visuals() -> void:
+	%DefaultVisuals.custom_minimum_size = Vector2(x,y)
 	if combatant:
-		pass
+		%DefaultVisuals.visible = false
+	else:
+		%DefaultVisuals.visible = true
 
 func show_selection_arrow() -> void:
-	%SelectionArrow.visible = true
+	#%SelectionArrow.visible = true
+	pass
 
 func hide_selection_arrow() -> void:
-	%SelectionArrow.visible = false
+	#%SelectionArrow.visible = false
+	pass
 
 func set_arrow_color(r : int, g : int, b : int) -> void:
 	r = clampi(r,0,255)
 	g = clampi(g,0,255)
 	b = clampi(b,0,255)
-	%SelectionArrow.modulate = Color(r,g,b,1)
+	#%SelectionArrow.modulate = Color(r,g,b,1)
 
 func play_animation(ability_name : String, direction : String) -> void:
 	%AnimationPlayer.play(combatant.combatant_name + ability_name + direction)
